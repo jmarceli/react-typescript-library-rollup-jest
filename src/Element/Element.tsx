e6e3144963ce76f1ceb354e67f2c1bb140bfc0f4
Jan Grzegorowski <grzegorowski@gmail.com>
2020-03-14 21:15:44 +0100

    Add styles bundling with CSS modules

--- src/Element/Element.tsx
+++ src/Element/Element.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { someFunc } from "./someFunc";
 
+import styles from "./Element.module.scss";
+
 export const Element: React.FC = () => {
   const b = someFunc();
-  return <div>{b}</div>;
+  return <div className={styles.root}>{b}</div>;
 };
