f0e3d3e180b45d5bbf03d65bf81c9e6352c63cdf
Jan Grzegorowski <grzegorowski@gmail.com>
2020-03-15 00:00:24 +0100

    Add example/ dir for development and testing

---
+++ example/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Element } from "react-typescript-library-rollup-jest";
+
+const mountNode = document.getElementById("app");
+ReactDOM.render(<Element />, mountNode);
